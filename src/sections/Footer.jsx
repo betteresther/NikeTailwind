7d70e6adc9323731a1d2950d244ede414a8cbe64
Esther <125261036+betteresther@users.noreply.github.com>
2025-10-30 10:10:41 +0100

    Creation of Footer Section and halfway completion

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,37 @@
+import { footerLogo } from "../assets/images"
+import { socialMedia } from "../constants"
+
 const Footer = () => {
   return (
-    <div>
-      
+<footer className="max-container">
+<div className="flex justify-between items-start gap-20
+flex-wrap max-lg:flex-col">
+  <div className="flex flex-col items-start">
+    <a href="/">
+    <img 
+    src={footerLogo}
+     width={150}
+     height={46} />
+    </a>
+    <p className="text-white-400 text-base leading-7 font-montserrat
+    mt-6 sm:max-w-sm">
+      Get shoes ready for the new term at your nearest Nike 
+      store. Find Your perfect size In Store. Get Rewards
+    </p>
+    <div className="flex items-center gap-5 mt-8">
+      {socialMedia.map((icon) =>(
+        <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
+          <img src={icon.src}
+           alt={icon.alt} 
+           width={24}
+           height={24}/>
+        </div>
+      ))}
     </div>
+  </div>
+  
+</div>
+</footer>
   )
 }
 
